8bb9a76ddb63518c7041b7778904d61bf0904a2d
Vladimir <123@mail.ru>
2023-10-12 20:01:16 +0300

    +HW5+HW6

---
+++ Seminar5/HomeWork5/task36/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих 
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+using System;
+
+public class Answer
+{
+    public static int SumOddElements(int[] array)
+    {
+// Введите свое решение ниже
+        int sum = 0;
+        for (int i = 1; i < array.Length; i += 2)
+        {
+            sum += array[i];
+        }
+        return sum;
+
+    }
+
+    public static void PrintArray(int[] array)
+    {
+         // Введите свое решение ниже
+         foreach (int element in array)
+        {
+            Console.Write(element + "\t");
+        }
+        Console.WriteLine();
+
+    }
+
+
+// Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+        int[] array;
+        if (args.Length == 0)
+        {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            array = new int[] { 12, 64, 28, 93, 35, 47, 6, 72, 58, 21 };
+        }
+        else
+        {
+            string[] argStrings = args[0].Split(", ");
+            array = new int[argStrings.Length];
+            for (int i = 0; i < argStrings.Length; i++)
+            {
+                if (int.TryParse(argStrings[i], out int number))
+                {
+                    array[i] = number;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+                    return;
+                }
+            }
+        }
+
+        // Не удаляйте строки ниже
+        PrintArray(array);
+        int sumOdd = SumOddElements(array);
+        Console.WriteLine($"Сумма нечетных элементов: {sumOdd}");
+    }
+}
---
+++ Seminar6/HomeWork6/task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int count = 0;
+Console.WriteLine("Введите количество чисел:");
+int M = int.Parse(Console.ReadLine());
+for (int i = 0; i < M; i++)
+{
+    Console.Write($"введите число{i+1}:");
+    double num = double.Parse(Console.ReadLine());
+    if (num >0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел больше 0: {count}");
